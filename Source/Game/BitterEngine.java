73afbd483140417585517a0ad0abf9b998a6ace2
edzye101 <edzye101@gmail.com>
2016-12-11 16:13:34 +0100

    Just added the psuedocode

---
+++ Source/Game/BitterEngine.java
@@ -0,0 +1,32 @@
+public class BitterEngine {
+
+	String name;
+	String instructions = "You have  to color the graph as quickly as possible. However you can’t stop  until the minimum number of colors has been reached”;
+	Boolean GameOver = false;
+	int time;
+
+	// Display instruction screen with button to progresss to the game 
+	
+	// Begin game loop 
+
+     
+
+	 
+
+    /*
+		While(!GameOver) {
+		BitterGame.input()	-	Process Inputs (Ask User to select a position  )
+		
+		BitterGame.update()   -    Update the game state ( check whether or not the Chromatic number has been reached if so )
+
+		BitterGame.output()   -    Display Hints on the graph for where it should be and how 
+
+		
+			
+			
+		}
+		
+
+	*/	
+
+}
