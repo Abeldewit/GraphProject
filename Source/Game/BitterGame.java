da210bdb8d6a4180d8d00f56118978ddaefeffd8
edzye101 <edzye101@gmail.com>
2016-12-11 16:33:03 +0100

    added the BitterGame abstract file

---
+++ Source/Game/BitterGame.java
@@ -0,0 +1,31 @@
+public class BitterGame extends BitterEngine {
+
+
+    /*
+
+
+
+    */
+
+    //display statistics of the game
+
+
+    public void stats(){
+
+
+    }
+    // Ask for input from the player
+    public void input(){
+
+    }
+    // update the game state and check if the chromatic number has been reached
+    public void update(){
+
+
+    }
+    // put hints on the screen towards the right nodes.
+    public void output(){
+
+    }
+
+}
