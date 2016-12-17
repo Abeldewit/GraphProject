271e3c5e33a58aee12139caf89464d9dc4626a39
ProgramAbel <abelroef@hotmail.com>
2016-12-17 19:16:21 +0100

    Small changes
    Consle outputs when you've solved it and ends the game if you solved it
    with enough colors

--- Source/Clone/Drawer.java
+++ Source/Clone/Drawer.java
@@ -14,13 +14,14 @@ import java.util.*;
 
 public class Drawer extends JPanel{
 	
+
 	private static VertexShape[] vertecies;
 	private static EdgeShape[] edges;
 	private static int chromatic_number = 0;
 	private static int time = 60;
 	private static boolean gameOn = true;
-	private static int score = 0;
-	
+	static int score = 0;
+
 	private static JFrame frame;
 	private static JLabel timeField = new JLabel("   Time Left: "+time+" s");
 	private static JLabel chrField = new JLabel("   Chromatic Number: "+chromatic_number);
@@ -30,12 +31,13 @@ public class Drawer extends JPanel{
 	private static JButton startButton = new JButton("generate");
 	private static JPanel controlPanel;
 	private static JPanel graphicPanel;
-	
+
 	static int interval;
 	static Timer timer;
-	
+	static int greenEdges;
+
 	public Drawer() {
-		
+
 		graphicPanel = new JPanel(){
             @Override
             protected void paintComponent(Graphics g) {
@@ -46,42 +48,42 @@ public class Drawer extends JPanel{
 				}catch(IOException e) {e.printStackTrace();}
             }
         };
-		
+
 		controlPanel = new JPanel();
-		
+
 		class StartButtonListener implements ActionListener{
-			
+
 			public void actionPerformed(ActionEvent event) {
-				
+
 				Graphics g = graphicPanel.getGraphics();
 				Graphics2D g_2D = (Graphics2D) g;
-				
+
 				drawBackground();
-				
+
 				//collecting the input
 				int e = Integer.parseInt(edgeField.getText());
 				int v = Integer.parseInt(vertexField.getText());
-				
+
 				//generating random graph
 				graphGen randomgraph = new graphGen(v, e);
 				int[][] adjacencyMatrix = randomgraph.getMatrix();
-				
+
 				//bruteforcing solution
 				Force brute = new Force(v,e,adjacencyMatrix);
 				chromatic_number = brute.getChr();
 				System.out.println("CHR received");
 				chrField.setText("   Chromatic Number: "+chromatic_number);
-				
+
 				//defining the new set
 				GraphShape graph = new GraphShape(v,e,adjacencyMatrix);
 				vertecies = graph.getVertecies();
 				edges = graph.getEdges();
-				
+
 				//drawing the edges
 				for(int i=0;i<edges.length;i++){
 					drawEdge(edges[i]);
 				}
-				
+
 				//drawing the vertecies
 				for(int i=0;i<vertecies.length;i++){
 					drawVertex(vertecies[i]);
@@ -95,8 +97,25 @@ public class Drawer extends JPanel{
 
 				changeColor(event);
 				updateHint();
+
+				//checking whether the game should finish
+					boolean red = false;
+					for(int i=0;i<edges.length;i++) {
+						if(edges[i].getColor() == Color.red) {
+							red = true;
+						}
+					}
+					if(red == false) {
+						if(countUsedColors() - chromatic_number == 0) {
+							System.out.println("You've won with score: " + score);
+							gameOver();
+						} else {
+							System.out.println("Solved but to many colors");
+						}
+					}
+
 			}
-			
+
 			public void mouseReleased(MouseEvent event) {}
 			public void mouseClicked(MouseEvent event){}
 			public void mouseEntered(MouseEvent event){}
@@ -105,43 +124,43 @@ public class Drawer extends JPanel{
 		startButton.addActionListener(new StartButtonListener());
 		graphicPanel.addMouseListener(new MousePressListener());
 	}
-	
+
 	public static void drawBackground(){
-		
+
 		//accessing the graphicPanel
 		Graphics g = graphicPanel.getGraphics();
 		Graphics2D g_2D = (Graphics2D) g;
-		
+
 		g_2D.setStroke(new BasicStroke( 4.0F ));
-				
+
 		//redrawing empty background
 		try{
 			BufferedImage image = ImageIO.read(new File("resources/background5.jpeg"));
 			g.drawImage(image, 0, 0, null);
 		}catch(IOException ex) {ex.printStackTrace();}
-		
+
 	}
-	
+
 	public static void drawVertex( VertexShape vertex ){
-		
+
 		//accessing the graphicPanel
 		Graphics g = graphicPanel.getGraphics();
 		Graphics2D g_2D = (Graphics2D) g;
-		
+
 		//drawing shapes
 		g_2D.setColor(Color.black);
 		g_2D.draw(vertex.getShape());
 		g_2D.setColor(vertex.getColor());
 		g_2D.fill(vertex.getShape());
-		
+
 	}
-	
+
 	public void drawEdge( EdgeShape edge ){
-		
+
 		//accessing the graphicPanel
 		Graphics g = graphicPanel.getGraphics();
 		Graphics2D g_2D = (Graphics2D) g;
-		
+
 		//drawing shapes
 		g_2D.setStroke(new BasicStroke( 1.0F ));
 		if(edge.getVertexA().getColor() != edge.getVertexB().getColor()){
@@ -156,15 +175,15 @@ public class Drawer extends JPanel{
 			g_2D.setStroke(new BasicStroke( 4.0F ));
 		}
 	}
-	
+
 	public void changeColor(MouseEvent event){
-				
+
 		//accessing the graphic panel
 		Graphics g = graphicPanel.getGraphics();
 		Graphics2D g_2D = (Graphics2D) g;
-		
+
 		VertexShape vertexToChange = null;
-				
+
 		//drawing shapes
 		for(int i=0;i<vertecies.length;i++){
 			if(vertecies[i].withinBorders(event.getX(),event.getY())){
@@ -176,13 +195,13 @@ public class Drawer extends JPanel{
 			}
 		}
 	}
-	
+
 	public void updateHint(){
-		
+
 		//accessing the graphic panel
 		Graphics g = graphicPanel.getGraphics();
 		Graphics2D g_2D = (Graphics2D) g;
-		
+
 		//redrawing the shapes
 		for(int i=0;i<edges.length;i++){
 			drawEdge(edges[i]);
@@ -191,9 +210,9 @@ public class Drawer extends JPanel{
 			drawVertex(vertecies[i]);
 		}
 	}
-	
+
 	public static void startTimer(int start){
-		
+
 		int delay = 1000;
 		int period = 1000;
 		timer = new Timer();
@@ -209,27 +228,27 @@ public class Drawer extends JPanel{
 				if(interval ==0){
 				gameOver();
 				timer.cancel();
-				}	
+				}
 			}
 		}, delay, period);
 	}
-	
+
 	private static final int setInterval() {
 		return --interval;
 	}
-	
+
 	public static void gameOver(){
-		
+
 		System.out.println("GAME OVER");
 		frame.dispose();
 		new GameOver(100);
-		
+
 	}
-	
+
 	public static void updateScore(){
-		
+
 		boolean valid = true;
-		
+
 		for(int i=0;i<edges.length;i++){
 			if(edges[i].getColor() == Color.red){
 				score = 0;
@@ -244,10 +263,10 @@ public class Drawer extends JPanel{
 			scoreField.setText("   Score: "+score);
 		}
 	}
-	
+
 	public static int countUsedColors(){
 		ArrayList<Color> usedColours = new ArrayList<Color>();
-		
+
 		for(int i=0;i<vertecies.length;i++){
 			if(i==0){
 				usedColours.add(vertecies[0].getColor());
@@ -259,21 +278,21 @@ public class Drawer extends JPanel{
 		}
 		return (usedColours.size());
 	}
-	
+
 	public static void main(String args[]){
-		
+
 		Drawer d = new Drawer();
-		
+
 		JLabel vertexLabel = new JLabel("Vertecies:  ");
 		JLabel edgeLabel = new JLabel("    Edges:    ");
-		
+
 		vertexLabel.setForeground(Color.WHITE);
 		edgeLabel.setForeground(Color.WHITE);
 		timeField.setForeground(Color.WHITE);
 		chrField.setForeground(Color.WHITE);
 		scoreField.setForeground(Color.WHITE);
-		
-		
+
+
 		controlPanel = new JPanel(){
 			@Override
 			protected void paintComponent(Graphics g) {
@@ -285,7 +304,7 @@ public class Drawer extends JPanel{
 			}
 		};
 		controlPanel.setPreferredSize(new Dimension(150,150));
-		
+
 		controlPanel.add(vertexLabel);
 		controlPanel.add(vertexField);
 		controlPanel.add(edgeLabel);
@@ -294,19 +313,19 @@ public class Drawer extends JPanel{
 		controlPanel.add(chrField);
 		controlPanel.add(timeField);
 		controlPanel.add(scoreField);
-		
+
 		JPanel contentPanel = new JPanel(new BorderLayout());
 		contentPanel.add(controlPanel, BorderLayout.LINE_END);
 		contentPanel.add(graphicPanel, BorderLayout.CENTER);
 		contentPanel.setPreferredSize(new Dimension(900, 723));
-		
+
 		frame = new JFrame();
-		
+
 		frame.setSize(1100, 723);
 		frame.add(contentPanel);
 		frame.setVisible(true);
 		frame.setResizable(false);
-		
+
 		startTimer(70);
 	}
 }
--- Source/Clone/GraphShape.java
+++ Source/Clone/GraphShape.java
@@ -2,56 +2,56 @@ import java.awt.geom.*;
 import java.awt.*;
 
 public class GraphShape{
-	
+
 	private final int MAX_VERTECIES = 16;
-	
+
 	private final int MIDX = 550;
 	private final int MIDY = 330;
-	private final int radius = 300;
+	private final int radius = 250;
 	private final int originX = MIDX-radius;
 	private final int originY = MIDY-radius;
 	private final double[][] POSITIONS = {
-		
+
 		{MIDX,MIDY-radius},
 		{MIDX+0.4*radius,MIDY-0.9*radius},
 		{MIDX+0.7*radius,MIDY-0.7*radius},
 		{MIDX+0.9*radius,MIDY-0.4*radius},
-		
+
 		{MIDX+radius,MIDY},
 		{MIDX+0.9*radius,MIDY+0.4*radius},
 		{MIDX+0.7*radius,MIDY+0.7*radius},
 		{MIDX+0.4*radius,MIDY+0.9*radius},
-		
+
 		{MIDX,MIDY+radius},
 		{MIDX-0.4*radius,MIDY+0.9*radius},
 		{MIDX-0.7*radius,MIDY+0.7*radius},
 		{MIDX-0.9*radius,MIDY+0.4*radius},
-		
+
 		{MIDX-radius,MIDY},
 		{MIDX-0.9*radius,MIDY-0.4*radius},
 		{MIDX-0.7*radius,MIDY-0.7*radius},
 		{MIDX-0.4*radius,MIDY-0.9*radius},
-		
+
 	};
-	
+
 	private int edgeCount;
 	private int vertexCount;
 	private int[][] matrix;
-	
+
 	private VertexShape[] vertecies;
 	private EdgeShape[] edges;
 	private int posStep;
-	
+
 	public GraphShape(int vertexCount, int edgeCount, int[][] matrix){
-		
+
 		this.edgeCount = edgeCount;
 		this.vertexCount = vertexCount;
 		System.out.println("matrix received");
 		this.matrix = matrix;
 		posStep = MAX_VERTECIES / vertexCount;
-		
+
 		vertecies = new VertexShape[vertexCount];
-		
+
 		if(vertecies.length<9){
 			for(int j=0;j<vertecies.length;j++){
 				vertecies[j] = new VertexShape((int)POSITIONS[j*posStep][0],(int)POSITIONS[j*posStep][1]);
@@ -67,14 +67,14 @@ public class GraphShape{
 				f+=2;
 			}
 		}
-		
+
 		edges = new EdgeShape[edgeCount];
-		
+
 		int[][] lookupMatrix = matrix;
 		int edge_id = 0;
 		for(int i=0;i<matrix.length;i++){
 			for(int j=0;j<matrix[0].length;j++){
-				
+
 				if (lookupMatrix[i][j] == 1){
 					if(edge_id<edgeCount){
 						edges[edge_id] = new EdgeShape(vertecies[i],vertecies[j]);
@@ -86,8 +86,8 @@ public class GraphShape{
 			}
 		}
 	}
-	
-	
+
+
 	public VertexShape[] getVertecies(){
 		return vertecies;
 	}
